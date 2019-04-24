9e359d82d071dae8cf981f2cbec557ba01963a72
mvr <maxim.razin@gmail.com>
2019-04-24 06:57:27 -0700

    Added second round

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for i in $(seq 1 226)
 do 
-    wget -O raw/$i.html https://www.cvk.gov.ua/pls/vp2019/wp336pt001f01=719pt005f01=$i.html
+    #wget -O raw2019-p1/$i.html https://www.cvk.gov.ua/pls/vp2019/wp336pt001f01=719pt005f01=$i.html
+    wget -O raw2019-p2/$i.html https://cvk.gov.ua/pls/vp2019/wp336pt001f01=720pt005f01=$i.html
 done
